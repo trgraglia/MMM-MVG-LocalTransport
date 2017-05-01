64c367ac58931a36003d457491b7351a675f043f
Anthony Graglia <anthony.graglia@gmail.com>
2017-05-01 11:38:18 +0200

    Used moment JS to find the minute difference because the numbers were not correct with the last approach
    Difficult to debug the helper. All hints welcome.

--- node_helper.js
+++ node_helper.js
@@ -14,24 +14,31 @@ module.exports = NodeHelper.create({
     start: function () {
         console.log('--- ' + this.name + ': Node Helper Start');
     },
-    socketNotificationReceived: function (notification, payload) {
+    socketNotificationReceived: function (notification, config) {
         if (notification === 'MMM-MVG-GETDATA') {
             console.log('--- ' + this.name + ': Socket Notification Received: ' + notification);
-            this.getStationData(payload, this.processJson);
+
+            this.config = config;
+            this.getStationData(this.processJson);
         }
     },
     processJson: function (json) {
         console.log('--- ' + this.name + ': Process JSON');
 
         var departures = json['departures'];
-        var now = new Date();
+
+        if(this.config.debug) {
+            console.log(departures);
+        }
+
+        var now = moment();
         var items = {};
 
         for (var i = 0; i < departures.length; i++) {
             var departure = departures[i];
             var destination = departure['destination'];
-            var departureTime = new Date(departure['departureTime']);
-            var departureMinutes = Math.floor((Math.abs(departureTime - now) / 1000) / 60);
+            var departureTime = moment(departure['departureTime']);
+            var departureMinutes = moment.duration(departureTime.diff(now)).asMinutes();
 
             items[destination] = items[destination] || {
                     'label': departure['label'],
@@ -41,19 +48,26 @@ module.exports = NodeHelper.create({
             items[destination]['departureTimes'].push(departureMinutes);
         }
 
+        if(this.config.debug) {
+            console.log(items);
+        }
+
         return items;
     },
-    getStationData: function (config, callback) {
+    getStationData: function (callback) {
         console.log('--- ' + this.name + ': Get Station Data');
 
         var self = this;
 
         request({
-            uri: config.apiBase + '?station=' + config.id,
+            uri: self.config.apiBase + '?station=' + self.config.id,
             method: 'GET',
             json: true
         }, function (error, response, json) {
             if (!error && response.statusCode == 200) {
+                if(self.config.debug) {
+                    console.log(json);
+                }
                 var data = callback.call(self, json);
                 self.sendSocketNotification('MMM-MVG-DATARECEIVED', {id: this.id, data: data});
             } else {
@@ -62,6 +76,6 @@ module.exports = NodeHelper.create({
                 console.log('--- ' + 'response: ' + JSON.stringify(json));
                 console.log('--- ' + 'json: ' + JSON.stringify(json));
             }
-        }.bind({id: config.id}));
+        }.bind({id: self.config.id}));
     }
 });
